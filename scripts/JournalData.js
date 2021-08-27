2c810fe4e9c4bb2ecf3b62767f584d9b6d7857ab
Colby Ryan <colbyr.ryan@gmail.com>
2021-08-27 15:57:27 -0500

    Got javascript working and continued styling

---
+++ scripts/JournalData.js
@@ -0,0 +1,31 @@
+const journal = [
+    {
+        id: 1,
+        date: "08/27/2021",
+        concept: "HTML and CSS",
+        entry: "We talked about HTML components and how to make a grid layout with Flexbox in CSS",
+        mood: "Happy"
+    },
+    {
+        id: 2,
+        date: "07/26/2025",
+        concept: "Complex Flexbox",
+        entry: "I tried to have an element in my Flexbox layout also be another Flexbox layout. It hurt my brain. I hate Steve.",
+        mood: "Sad"
+    },
+    {
+        id: 2,
+        date: "07/26/2025",
+        concept: "Complex Flexbox",
+        entry: "I tried to have an element in my Flexbox layout also be another Flexbox layout. It hurt my brain. I hate Steve.",
+        mood: "Sad"
+    }
+]
+
+export const getJournalEntries = () => {
+    const sortedByDate = journal.sort(
+        (currentEntry, nextEntry) =>
+            Date.parse(currentEntry.date) - Date.parse(nextEntry.date)
+    )
+        return sortedByDate
+}
---
+++ scripts/JournalEntryList.js
@@ -0,0 +1,21 @@
+/*
+ *  Purpose:
+ *    To render as many journal entry components as
+ *    there are items in the collection exposed by the
+ *    data module component
+ */
+import { JournalEntryComponent } from "./JournalEntry.js";
+import { getJournalEntries } from "./JournalData.js";
+
+// DOM reference to where all entries will be rendered
+const entryLog = document.querySelector('.entryLog');
+
+export const  EntryListComponent = () => {
+     // Use the journal entry data from the data module component
+    const entries = getJournalEntries()
+
+    for (const entry of entries) {
+        //Invoke the component that returns an HTML rep of a single entry
+        entryLog.innerHTML += JournalEntryComponent(entry)
+    };
+}
