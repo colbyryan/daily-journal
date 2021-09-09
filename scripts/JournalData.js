778ce1490dc3d3501c4fddbdfbf65a4585365ce6
Colby Ryan <colbyr.ryan@gmail.com>
2021-09-09 15:22:58 -0500

    added more entries and js

--- scripts/JournalData.js
+++ scripts/JournalData.js
@@ -6,6 +6,27 @@ const journal = [
         concept: "HTML and CSS",
         entry: "We talked about HTML components and how to make a grid layout with Flexbox in CSS",
         mood: "Happy"
+    },
+    {
+        id: 1,
+        date: "08/27/2021",
+        concept: "HTML and CSS",
+        entry: "We talked about HTML components and how to make a grid layout with Flexbox in CSS",
+        mood: "Happy"
+    },
+    {
+        id: 1,
+        date: "08/27/2021",
+        concept: "HTML and CSS",
+        entry: "We talked about HTML components and how to make a grid layout with Flexbox in CSS",
+        mood: "Happy"
+    },
+    {
+        id: 1,
+        date: "08/27/2021",
+        concept: "HTML and CSS",
+        entry: "We talked about HTML components and how to make a grid layout with Flexbox in CSS",
+        mood: "Happy"
     }
 ]
 
@@ -16,3 +37,4 @@ export const getJournalEntries = () => {
     )
         return sortedByDate
 }
+
